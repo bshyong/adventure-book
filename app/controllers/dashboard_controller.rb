a6ed2f37297a2e63109ba986daceb98bf707cda1
Ben Shyong <likeachild@gmail.com>
2012-07-21 23:41:22 -0400

    styling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,6 +15,8 @@ class DashboardController < ApplicationController
     @user = FbGraph::User.fetch('bshyong', :access_token => current_user.access_token )   
 
     @photos = FbGraph::Query.new('SELECT pid, src_big, caption, created FROM photo WHERE pid IN (SELECT pid FROM photo_tag WHERE subject=me()) AND pid IN ( SELECT pid FROM photo_tag WHERE subject=1248270527)').fetch(current_user.access_token)
+    @events =  FbGraph::Query.new('select name, start_time, pic, description,location from event where eid in (select eid from event_member where uid=1084230387)').fetch(current_user.access_token) 
+
 
     render :layout => false
   end
