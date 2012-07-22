d464bd5aeb13e701606e2746aad799c6b1cb370c
Ben Shyong <likeachild@gmail.com>
2012-07-22 07:20:22 -0400

    random spacing helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,9 @@ module ApplicationHelper
   def fb_graph_sample_url
     'http://github.com/nov/fb_graph_sample'
   end
+      
+  def random_spacing
+     (rand(4)*500) * (rand() < 0.5 ? -1 : 1)
+  end
 
 end
